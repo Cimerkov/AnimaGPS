ddc989d56040e1383586873a4093936c6e1d7c67
Cimerkov <cimerkov@hotmail.com>
2022-06-27 13:59:32 +0200

    Anima Navbar

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Anima GPS</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="sticky top-0 flex bg-black">
+        <div class="logo w-1/2 m-5">
+            <img class="w-1/2" src="../practice/images/animagps-logo-white.png" alt="logo">
+        </div>
+        <div class="w-1/2 flex items-center justify-end">
+            <ul class="flex">
+                <li> <a class="mr-2.5" href="#">Home</a></li>
+                <li> <a class="mr-2.5" href="#">About US</a></li>
+                <li> <a class="mr-2.5" href="#">Features</a></li>
+                <li> <a class="mr-2.5" href="#">Clients</a></li>
+                <li><a class="mr-2.5" href="#">Contact Us</a></li>
+            </ul>
+            <a href="https://gps.anima.com.mk/login"><button class="mr-5 py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+                    Log In
+                </button></a>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
